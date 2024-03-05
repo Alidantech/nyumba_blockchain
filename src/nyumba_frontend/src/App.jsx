44aaec43cc7b99dc8c39ac607ab61af4fe1369c3
PETER IRUNGU <peteralidante254@gmail.com>
2024-03-05 03:15:37 +0300

    create frontend file structure

--- src/nyumba_frontend/src/App.jsx
+++ src/nyumba_frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { nyumba_backend } from 'declarations/nyumba_backend';
+import React from 'react';
+import { AppBar, Container, CssBaseline, Toolbar, Typography, Paper } from '@mui/material';
 
 function App() {
   const [greeting, setGreeting] = useState('');
@@ -15,15 +17,37 @@ function App() {
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">My Awesome Page</Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="lg" style={{ marginTop: '20px' }}>
+        <Paper elevation={3} style={{ padding: '20px' }}>
+          <Typography variant="h4" gutterBottom>
+            Welcome to My Page
+          </Typography>
+          <form action="#" onSubmit={handleSubmit}>
+            <label htmlFor="name">Enter your name: &nbsp;</label>
+            <input id="name" alt="Name" type="text" />
+            <button type="submit">Click Me!</button>
+          </form>
+          <section id="greeting">{greeting}</section>
+          <Typography variant="body1">
+            This is a simple example page using Material-UI. You can customize and expand it based
+            on your project requirements.
+          </Typography>
+        </Paper>
+      </Container>
+
+      <footer style={{ marginTop: '20px', padding: '10px', backgroundColor: '#f5f5f5' }}>
+        <Typography variant="body2" align="center">
+          © {new Date().getFullYear()} My Page. All rights reserved.
+        </Typography>
+      </footer>
+
     </main>
   );
 }
